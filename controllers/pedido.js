23cba747bdc14f3ee75a3315db27d4ceed8e212f
Pedro Castro <ppachecodecastro@gmail.com>
2020-01-24 17:54:40 -0300

    000

--- controllers/pedido.js
+++ controllers/pedido.js
@@ -1,83 +1,12 @@
-module.exports = function(io){
-   var pedido = {}
-   var ObjectID = require('mongodb').ObjectID;
+var ObjectID = require('mongodb').ObjectID;
 
-   pedido.listarPin = function (req, res) {
-      var userPin = req.params.pin;
-      console.log(userPin);
-       function callback(err, result) {
-           if (err) {
-               return res.sendStatus(503);
-           };
-           res.send(result);
-       }
-       req.db.collection('contas').findOne({"pin": Number(userPin)}, callback);
-   };
+ exports.listarCat = function (req, res) {
+      function callback(err, result) {
+          if (err) {
+              return res.sendStatus(503);
+          };
 
-   pedido.listar = function (req, res) {
-       function callback(err, result) {
-           if (err) {
-               return res.sendStatus(503);
-           };
-
-           res.send(result);
-       }
-       req.db.collection('contas').find().toArray(callback);
-   };
-
-   pedido.criar = function (req, res) {
-       function callback(err, result) {
-           if (err) {
-               return res.sendStatus(503);
-           }
-           res.sendStatus(201);
-       }
-
-       req.db.collection('contas').save(req.body, callback);
-   };
-
-
-   pedido.pedido = function (req, res) {
-     var userPin = req.params.pin;
-
-     req.db.collection('contas').update({"pin": Number(userPin)}, { "$push": {pedidos: req.body} }, function(err, result) {
-       if (err) {
-         return res.sendStatus(503);
-       }
-
-       res.send(result);
-     });
-
-     io.emit('novopedido', userPin)
-   };
-
-   pedido.fechar = function (req, res) {
-     var userPin = req.params.pin;
-
-     req.db.collection('contas').update({"pin": Number(userPin)}, { "$set": {fechado: true} }, function(err, result) {
-       if (err) {
-         return res.sendStatus(503);
-       }
-
-       res.send(result);
-     });
-     io.emit('fechar', userPin)
-
-   };
-
-   pedido.atualizarQuant = function (req, res) {
-      var userPin = req.params.pin;
-     var prod = req.params.prod;
-
-     req.db.collection('contas').update({"pin": Number(userPin), "pedidos.nome":prod}, {"$inc": { "pedidos.$.quantidade":1}}, function(err, result) {
-       if (err) {
-         return res.sendStatus(503);
-       }
-
-       res.send(result);
-     });
-     io.emit('inc', userPin)
-
-   };
-   return pedido
-}
+          res.send(result);
+      }
+      return req.db.collection('receitas').find('categoria').toArray(callback);
+  };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var expressMongoDb = require('express-mongo-db');
 var cors = require('cors');
 
 var basicAuth = require('basic-auth');
-var stripe = require('stripe')('sk_test_tXbWRqZLz0Wg21W6emKjQ4y4'); //paste your test secret here
+// var stripe = require('stripe')('sk_test_tXbWRqZLz0Wg21W6emKjQ4y4'); 
 
 // inicializa o express
 var app = express();
@@ -12,17 +12,17 @@ var router = express.Router();//stripe
 
 //socket.io
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var io = require('socket.io')(http);
 
-var pedidoController = require('./controllers/pedido.js')(io);
-var usuariosController = require('./controllers/usuarios.js');
+var pedidoController = require('./controllers/pedido.js');
+// var usuariosController = require('./controllers/usuarios.js');
 
 // inicializa o body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false })); //stripe
+// app.use(bodyParser.urlencoded({ extended: false })); //stripe
 
 // inicializa mongo e expoe para o express
-app.use(expressMongoDb('mongodb://localhost:27017/contas'));
+app.use(expressMongoDb('mongodb://localhost:27017/receitas'));
 app.use(cors());
 
 // inicializa o servidor na porta especificada
@@ -30,10 +30,10 @@ app.use(cors());
 //    console.log('Servidor inicializado');
 // });
 
-//socket.io
-io.on('connection', function(socket){
- console.log('a user connected');
-});
+// //socket.io
+// io.on('connection', function(socket){
+//  console.log('a user connected');
+// });
 
 http.listen(3001, function(){ //socket
   console.log('listening on *:3001');
@@ -45,23 +45,23 @@ app.use(function(req, res, next) {
     next();
 });
 
-// stripe
-app.use(router.post('/processpay', function (request, response) {
-    var stripetoken = request.body.stripetoken;
-    var amountpayable = request.body.amount;
-    var charge = stripe.charge.create({
-        amount: amountpayable,
-        currency: 'usd',
-        description: 'Sample transaction',
-        source: stripetoken
-    }, function (err, charge) {
-        if (err)
-            console.log(err);
-        else
-            response.send({ success: true });
-    })
-})
-);
+// // stripe
+// app.use(router.post('/processpay', function (request, response) {
+//     var stripetoken = request.body.stripetoken;
+//     var amountpayable = request.body.amount;
+//     var charge = stripe.charge.create({
+//         amount: amountpayable,
+//         currency: 'usd',
+//         description: 'Sample transaction',
+//         source: stripetoken
+//     }, function (err, charge) {
+//         if (err)
+//             console.log(err);
+//         else
+//             response.send({ success: true });
+//     })
+// })
+// );
 
 var auth = function (req, res, next) {
   function unauthorized(res) {
@@ -89,14 +89,16 @@ req.db.collection('usuarios').findOne(req.body, function(err, result) {
 
 // Endpoints
 //pedidos
-app.get('/contas/:pin', pedidoController.listarPin);
-app.get('/contas', pedidoController.listar);
-app.post('/contas', pedidoController.criar);
-app.put('/contas/:pin', pedidoController.pedido);
-app.put('/contas/:pin/add/:prod', pedidoController.atualizarQuant);
-app.put('/contas/fechar/:pin', pedidoController.fechar);
+app.get('/contas', pedidoController.listarCat);
+// app.get('/contas', pedidoController.listarPratos);
+// app.post('/contas', pedidoController.criar);
+// app.put('/contas/:pin', pedidoController.pedido);
+// app.put('/contas/:pin/add/:prod', pedidoController.atualizarQuant);
+// app.put('/contas/fechar/:pin', pedidoController.fechar);
 
-//usuario
-app.post('/usuarios', usuariosController.criarUser);
-app.get('/all/usuarios', usuariosController.listarUsers);
-app.get('/usuarios', auth, usuariosController.logUser);
+// //usuario
+// app.post('/usuarios', usuariosController.criarUser);
+// app.get('/all/usuarios', usuariosController.listarUsers);
+// app.get('/usuarios', auth, usuariosController.logUser);
+// db.receitas.insert( { _id: 2, base: "Arroz", principal: "Tofu", gbase: 100, gprincipal: 85, caterogia: "vegano", nome: "Tofu com Arroz" } )
+// db.receitas.insert( { _id: 3, base: "Arroz", principal: "Ovo", gbase: 100, gprincipal: 78, caterogia: "vegetariano", nome: "Tofu com Arroz" } )
